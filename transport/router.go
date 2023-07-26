53466fea259d858b11a59a05d92fc6006912d8a8
ynuraddi <definston@gmail.com>
2023-07-26 17:58:38 +0600

    fix: method of route findSubstring

--- transport/router.go
+++ transport/router.go
@@ -5,7 +5,7 @@ import "github.com/labstack/echo/v4"
 func (s *Server) setupRouter() {
 	router := echo.New()
 
-	router.GET("/rest/substr/find", s.findSubstring)
+	router.POST("/rest/substr/find", s.findSubstring)
 
 	s.router = router
 }
