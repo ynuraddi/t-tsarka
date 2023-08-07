4104ad9dfee8cffc68f682e92c36831ccf4c2964
ynuraddi <definston@gmail.com>
2023-08-07 14:42:37 +0600

    refactor: service user now is more clearly

--- service/user/user.go
+++ service/user/user.go
@@ -1,32 +1,29 @@
 package user
 
 import (
-	"context"
-
-	"github.com/ynuraddi/t-tsarka/model"
 	"github.com/ynuraddi/t-tsarka/repository"
 )
 
 type userService struct {
-	repo repository.IUserRepository
+	repository.IUserRepository
 }
 
 func New(repo repository.IUserRepository) *userService {
-	return &userService{repo: repo}
+	return &userService{IUserRepository: repo}
 }
 
-func (s *userService) Create(ctx context.Context, user model.User) (id int64, err error) {
-	return s.repo.Create(ctx, user)
-}
+// func (s *userService) Create(ctx context.Context, user model.User) (id int64, err error) {
+// 	return s.repo.Create(ctx, user)
+// }
 
-func (s *userService) Get(ctx context.Context, id int64) (user model.User, err error) {
-	return s.repo.Get(ctx, id)
-}
+// func (s *userService) Get(ctx context.Context, id int64) (user model.User, err error) {
+// 	return s.repo.Get(ctx, id)
+// }
 
-func (s *userService) Update(ctx context.Context, user model.User) (dbuser model.User, err error) {
-	return s.repo.Update(ctx, user)
-}
+// func (s *userService) Update(ctx context.Context, user model.User) (dbuser model.User, err error) {
+// 	return s.repo.Update(ctx, user)
+// }
 
-func (s *userService) Delete(ctx context.Context, id int64) error {
-	return s.repo.Delete(ctx, id)
-}
+// func (s *userService) Delete(ctx context.Context, id int64) error {
+// 	return s.repo.Delete(ctx, id)
+// }
